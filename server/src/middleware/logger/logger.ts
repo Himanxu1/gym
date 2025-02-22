19de85e2a37a343b9baa777b09824a471a678130
himanxu2 <himanshu.singh@wherehouse.io>
2025-02-22 20:38:44 +0530

    set up logger

---
+++ server/src/middleware/logger/logger.ts
@@ -0,0 +1,27 @@
+import * as logger from "winston";
+
+const format = logger.format.combine(
+  logger.format.colorize({ all: false }),
+  logger.format.timestamp(),
+  logger.format.printf((info) => {
+    return `[${info.timestamp}] ${info.level}: ${info.message}`;
+  })
+);
+
+logger.remove(logger.transports.Console);
+
+logger.addColors({
+  debug: "green",
+  info: "cyan",
+  silly: "magenta",
+  warn: "yellow",
+  error: "red",
+});
+
+logger.configure({
+  level: "info",
+  format,
+  transports: [new logger.transports.Console()],
+});
+
+export { logger };
